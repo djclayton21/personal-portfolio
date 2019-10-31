719b1e587e3d182982862c5bccf423979565d562
Dan Clayton <clayton.daniel.j@gmail.com>
2019-10-30 20:15:29 -0600

    polish

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -5,16 +5,19 @@ const Intro = (props) => {
     return (
         <section className='intro' id="intro" >
                 <div>
-                    <h1>Welcome</h1>
+                    <h1>Welcome!</h1>
                     <hr/>
                 </div>
             <div className="intro-welcome" >
-                <h2>I'm Dan Clayton, and this is my portfolio.</h2>
-                <p>I'm a full-stack web developer who loves to learn and grow by solving problems.</p>
-                <p>Please take a look around, and feel free to contact me if you have any questions!</p>
+                <h2>My name is Dan Clayton, and this is my portfolio.</h2>
+                <p>I'm a freshly-minted developer who loves to learn by bulding.</p>
+                <p>If you have any questions or suggestions, I'd love to hear from you through
+                    <a href="https://www.linkedin.com/in/djclayton21/" target="_blank" rel="noopener noreferrer" > LinkedIn <i className="fas fa-link"></i> </a>
+                    or by <a href="mailto:clayton.daniel.j@gmail.com">email <i class="fas fa-envelope"></i>.</a> Please let me know what you think!
+                </p>
             </div>
             <i className="fas fa-chevron-down"></i>
         </section>);
 }
- 
+
 export default Intro;
--- src/data/projects/projects.js
+++ src/data/projects/projects.js
@@ -4,7 +4,7 @@ import scatterbrainImg from './images/scatterbrain.png'
 export const projects = [
     {
         name: 'Stock Market React App',
-        description: 'Website for Stock Market price information and company data',
+        description: 'Website for Stock Market price information and company data. My first React app using an external api.',
         toolsUsed: 'React, axios, JSX, CSS',
         image: stocksImg,
         liveUrl: 'http://dcstockmarket.surge.sh/',
@@ -12,7 +12,7 @@ export const projects = [
     },
     {
         name: 'Scatterbrain Study Tool',
-        description: 'Full-stack CRUD web app to manage study topics using spaced learning.',
+        description: 'Full-stack CRUD web app to manage study topics using spaced learning. My first web app using the MERN stack, as well as my first collaborative project.',
         toolsUsed: 'MongoDB, Express, React, Node',
         image: scatterbrainImg,
         liveUrl: 'https://scatterbrainstudytool.herokuapp.com/',
